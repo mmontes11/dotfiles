1927614cf9fa91118329dad58f2509108d89654f
Martin Montes <martin11lrx@gmail.com>
2023-11-28 17:30:40 +0100

    Bump yq

--- scripts/bin.sh
+++ scripts/bin.sh
@@ -3,7 +3,7 @@
 source ./scripts/lib.sh
 
 # yq
-YQ_VERSION=v4.18.1
+YQ_VERSION=v4.40.3
 YQ_URL=https://github.com/mikefarah/yq/releases/download/$YQ_VERSION/yq_linux_amd64
 
 install_bin yq $YQ_URL
