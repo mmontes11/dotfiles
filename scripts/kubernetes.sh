dc365d965ad78e4fe7f3ad209f78c7bc16c409c8
Martin Montes <martin11lrx@gmail.com>
2024-04-08 14:38:04 +0200

    Bump k8s scripts

--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-KUBE_SCRIPTS_VERSION=v0.0.11
+KUBE_SCRIPTS_VERSION=v0.0.15
 curl -sfL https://raw.githubusercontent.com/mmontes11/k8s-scripts/$KUBE_SCRIPTS_VERSION/kubernetes.sh | bash -s -
