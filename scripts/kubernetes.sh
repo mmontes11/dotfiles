a45f04ff5fefe08a22bbda1b1ad4c9573a7c9af1
Martin Montes <martin11lrx@gmail.com>
2024-05-17 19:30:28 +0200

    Updated k8s-tooling

--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -2,5 +2,4 @@
 
 set -euo pipefail
 
-KUBE_SCRIPTS_VERSION=v0.0.19
-curl -sfL https://raw.githubusercontent.com/mmontes11/k8s-scripts/$KUBE_SCRIPTS_VERSION/kubernetes.sh | bash -s -
+curl -sfL https://raw.githubusercontent.com/mmontes11/k8s-tooling/main/kubernetes.sh | bash -s -
