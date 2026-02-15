31a2f438794bda631b088854b07ee41dc019143d
Martin Montes <martin11lrx@gmail.com>
2026-02-15 13:10:41 +0100

    Added opencode

--- scripts/ai.sh
+++ scripts/ai.sh
@@ -7,9 +7,9 @@ UV_VERSION=${UV_VERSION:-0.9.26}
 curl -fsSL https://astral.sh/uv/${UV_VERSION}/install.sh | bash
 
 echo "Installing ollama..."
-OLLAMA_VERSION=${OLLAMA_VERSION:-0.15.1}
+OLLAMA_VERSION=${OLLAMA_VERSION:-0.16.1}
 curl -fsSL https://ollama.com/install.sh | OLLAMA_VERSION=${OLLAMA_VERSION} bash
 
-echo "Installing claude..."
-CLAUDE_VERSION=${CLAUDE_VERSION:-2.1.7}
-curl -fsSL https://claude.ai/install.sh | bash -s -- ${CLAUDE_VERSION}
+echo "Installing opencode..."
+OPENCODE_VERSION=${CLAUDE_VERSION:-1.2.4}
+curl -fsSL  https://opencode.ai/install | VERSION=${OPENCODE_VERSION} bash
