633d3af4bd8241eda8ab2dec9cd2cd9f82d16b54
Martin Montes <martin11lrx@gmail.com>
2026-06-14 18:44:11 +0000

    Decomission ollama

--- scripts/ai.sh
+++ scripts/ai.sh
@@ -6,10 +6,6 @@ echo "Installing uv..."
 UV_VERSION=${UV_VERSION:-0.9.26}
 curl -fsSL https://astral.sh/uv/${UV_VERSION}/install.sh | bash
 
-echo "Installing ollama..."
-OLLAMA_VERSION=${OLLAMA_VERSION:-0.16.1}
-curl -fsSL https://ollama.com/install.sh | OLLAMA_VERSION=${OLLAMA_VERSION} bash
-
 echo "Installing opencode..."
 OPENCODE_VERSION=${OPENCODE_VERSION:-1.2.4}
 curl -fsSL  https://opencode.ai/install | VERSION=${OPENCODE_VERSION} bash
